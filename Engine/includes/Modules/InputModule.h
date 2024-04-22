d68a23192485578dfad958a8d18d2492d7c8c2b4
BySkullZ <bahrimomo061@gmail.com>
2024-04-22 12:52:49 +0200

    callbacks a rajouter

--- Engine/includes/Modules/InputModule.h
+++ Engine/includes/Modules/InputModule.h
@@ -3,8 +3,12 @@
 
 #include <unordered_map>
 #include <GLFW/glfw3.h>
+#include <map>
+#include <glm.hpp>
 
+#include "InputKeyCode.h"
 #include "Module.h"
+#include "Modules/WindowModule.h"
 
 class InputModule final : public Module
 {
@@ -18,9 +22,17 @@ class InputModule final : public Module
 		bool IsMouseButtonPressed(int _button);
 		void GetMousePosition(double& _xPos, double& _yPos) const;
 
-		i32 GetKeyDown(KeyCode keyCode, bool bIgnoreImGui = false) const;
-		bool GetKeyPressed(KeyCode keyCode, bool bIgnoreImGui = false) const;
-		bool GetKeyReleased(KeyCode keyCode, bool bIgnoreImGui = false) const;
+		int32_t GetKeyDown(Inputs::KeyCode keyCode, bool bIgnoreImGui = false) const;
+		bool GetKeyPressed(Inputs::KeyCode keyCode, bool bIgnoreImGui = false) const;
+		bool GetKeyReleased(Inputs::KeyCode keyCode, bool bIgnoreImGui = false) const;
+
+		void ClearAllInputs();
+		void ClearMouseInput();
+		void ClearKeyboadInput();
+
+		glm::vec2 GetMousePosition() const;
+
+		void KeyCallback(Inputs::KeyCode keyCode, Inputs::KeyAction keyAction, int32_t mods);
 
 		/**
 			* @brief Initialise le module.
@@ -73,10 +85,21 @@ class InputModule final : public Module
 		void Finalize() override;
 
 	private:
-		GLFWwindow*                   window;
-		std::unordered_map<int, bool> keys;
+		WindowModule*                   windowModule = nullptr;
+		std::unordered_map<Inputs::KeyCode, Inputs::Key> keys;
 		std::unordered_map<int, bool> mouseButtons;
 		double                        mouseX, mouseY;
+
+		static const int32_t MOUSE_BUTTON_COUNT = (int32_t)Inputs::MouseButton::_NONE;
+
+		uint32_t m_MouseButtonStates;
+		uint32_t m_MouseButtonsPressed;
+		uint32_t m_MouseButtonsReleased;
+		Inputs::MouseDrag m_MouseButtonDrags[MOUSE_BUTTON_COUNT];
+		glm::vec2 m_MousePosition = { 0, 0 };
+		glm::vec2 m_PrevMousePosition = { 0, 0 };
+		float m_ScrollXOffset = 0;
+		float m_ScrollYOffset = 0;
 
 		static void KeyCallback(GLFWwindow* _window, int _key, int _scancode, int _action, int _mods);
 		static void MouseButtonCallback(GLFWwindow* _window, int _button, int _action, int _mods);
--- Engine/sources/Modules/InputModule.cpp
+++ Engine/sources/Modules/InputModule.cpp
@@ -1,5 +1,4 @@
 #include "Modules/InputModule.h"
-#include "Modules/WindowModule.h"
 #include "ModuleManager.h"
 #include <iostream>
 #include <imgui.h>
@@ -9,111 +8,111 @@ InputModule::~InputModule()
 
 }
 
-void InputModule::ProcessInput()
-{
-	// Reset mouse position
-	mouseX = 0.0;
-	mouseY = 0.0;
-
-	// Process joystick and gamepad input
-	for (int jid = GLFW_JOYSTICK_1; jid <= GLFW_JOYSTICK_LAST; ++jid)
-	{
-		if (glfwJoystickPresent(jid))
-		{
-			if (const float* axes = glfwGetJoystickAxes(jid, nullptr))
-				// Process joystick axes
-				for (int i = 0; i < 6; ++i)
-				{
-					if (axes[i] != 0.0)
-					{
-						// Example: process joystick axis input
-					}
-				}
-
-			if (const unsigned char* buttons = glfwGetJoystickButtons(jid, nullptr))
-				// Process joystick buttons
-				for (int i = 0; i < 16; ++i)
-				{
-					if (buttons[i] == GLFW_PRESS)
-					{
-						// Example: process joystick button input
-					}
-				}
-
-			if (const unsigned char* hats = glfwGetJoystickHats(jid, nullptr))
-				// Process joystick hats
-				for (int i = 0; i < 4; ++i)
-				{
-					if (hats[i] != GLFW_HAT_CENTERED)
-					{
-						// Example: process joystick hat input
-					}
-				}
-		}
-	}
-
-	glfwPollEvents();
-}
-
-
-bool InputModule::IsKeyPressed(const int _key)
-{
-	return keys[_key];
-}
-
-bool InputModule::IsMouseButtonPressed(const int _button)
-{
-	return mouseButtons[_button];
-}
-
-void InputModule::GetMousePosition(double& _xPos, double& _yPos) const
-{
-	_xPos = mouseX;
-	_yPos = mouseY;
-}
-
-void InputModule::KeyCallback(GLFWwindow* _window, const int _key, int _scancode, const int _action, int _mods)
-{
-	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
-
-	if (_action == GLFW_PRESS) input_module->keys[_key] = true;
-	else if (_action == GLFW_RELEASE) input_module->keys[_key] = false;
-}
-
-void InputModule::MouseButtonCallback(GLFWwindow* _window, const int _button, const int _action, int _mods)
-{
-	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
-
-	if (_action == GLFW_PRESS) input_module->mouseButtons[_button] = true;
-	else if (_action == GLFW_RELEASE) input_module->mouseButtons[_button] = false;
-}
-
-void InputModule::CursorPositionCallback(GLFWwindow* _window, const double _xPos, const double _yPos)
-{
-	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
-
-	input_module->mouseX = _xPos;
-	input_module->mouseY = _yPos;
-}
-
-void InputModule::JoystickCallback(const int _jid, const int _event)
-{
-	if (_event == GLFW_CONNECTED) std::cout << "Joystick connected: " << _jid << std::endl;
-	else if (_event == GLFW_DISCONNECTED) std::cout << "Joystick disconnected: " << _jid << std::endl;
-}
-
-void InputModule::GamepadInput(int _jid, const GLFWgamepadstate* _state)
-{
-	// Example: Process gamepad input
-	if (_state->buttons[GLFW_GAMEPAD_BUTTON_A] == GLFW_PRESS)
-	{
-		// Button A pressed
-	}
-
-	// Example: Use gamepad axes
-	float left_stick_x = _state->axes[GLFW_GAMEPAD_AXIS_LEFT_X];
-	float left_stick_y = _state->axes[GLFW_GAMEPAD_AXIS_LEFT_Y];
-}
+//void InputModule::ProcessInput()
+//{
+//	// Reset mouse position
+//	mouseX = 0.0;
+//	mouseY = 0.0;
+//
+//	// Process joystick and gamepad input
+//	for (int jid = GLFW_JOYSTICK_1; jid <= GLFW_JOYSTICK_LAST; ++jid)
+//	{
+//		if (glfwJoystickPresent(jid))
+//		{
+//			if (const float* axes = glfwGetJoystickAxes(jid, nullptr))
+//				// Process joystick axes
+//				for (int i = 0; i < 6; ++i)
+//				{
+//					if (axes[i] != 0.0)
+//					{
+//						// Example: process joystick axis input
+//					}
+//				}
+//
+//			if (const unsigned char* buttons = glfwGetJoystickButtons(jid, nullptr))
+//				// Process joystick buttons
+//				for (int i = 0; i < 16; ++i)
+//				{
+//					if (buttons[i] == GLFW_PRESS)
+//					{
+//						// Example: process joystick button input
+//					}
+//				}
+//
+//			if (const unsigned char* hats = glfwGetJoystickHats(jid, nullptr))
+//				// Process joystick hats
+//				for (int i = 0; i < 4; ++i)
+//				{
+//					if (hats[i] != GLFW_HAT_CENTERED)
+//					{
+//						// Example: process joystick hat input
+//					}
+//				}
+//		}
+//	}
+//
+//	glfwPollEvents();
+//}
+//
+//
+//bool InputModule::IsKeyPressed(const int _key)
+//{
+//	return false;//keys[_key];
+//}
+//
+//bool InputModule::IsMouseButtonPressed(const int _button)
+//{
+//	return mouseButtons[_button];
+//}
+//
+//void InputModule::GetMousePosition(double& _xPos, double& _yPos) const
+//{
+//	_xPos = mouseX;
+//	_yPos = mouseY;
+//}
+//
+//void InputModule::KeyCallback(GLFWwindow* _window, const int _key, int _scancode, const int _action, int _mods)
+//{
+//	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
+//
+//	if (_action == GLFW_PRESS) input_module->keys[_key] = true;
+//	else if (_action == GLFW_RELEASE) input_module->keys[_key] = false;
+//}
+//
+//void InputModule::MouseButtonCallback(GLFWwindow* _window, const int _button, const int _action, int _mods)
+//{
+//	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
+//
+//	if (_action == GLFW_PRESS) input_module->mouseButtons[_button] = true;
+//	else if (_action == GLFW_RELEASE) input_module->mouseButtons[_button] = false;
+//}
+//
+//void InputModule::CursorPositionCallback(GLFWwindow* _window, const double _xPos, const double _yPos)
+//{
+//	const auto input_module = static_cast<InputModule*>(glfwGetWindowUserPointer(_window));
+//
+//	input_module->mouseX = _xPos;
+//	input_module->mouseY = _yPos;
+//}
+//
+//void InputModule::JoystickCallback(const int _jid, const int _event)
+//{
+//	if (_event == GLFW_CONNECTED) std::cout << "Joystick connected: " << _jid << std::endl;
+//	else if (_event == GLFW_DISCONNECTED) std::cout << "Joystick disconnected: " << _jid << std::endl;
+//}
+//
+//void InputModule::GamepadInput(int _jid, const GLFWgamepadstate* _state)
+//{
+//	// Example: Process gamepad input
+//	if (_state->buttons[GLFW_GAMEPAD_BUTTON_A] == GLFW_PRESS)
+//	{
+//		// Button A pressed
+//	}
+//
+//	// Example: Use gamepad axes
+//	float left_stick_x = _state->axes[GLFW_GAMEPAD_AXIS_LEFT_X];
+//	float left_stick_y = _state->axes[GLFW_GAMEPAD_AXIS_LEFT_Y];
+//}
 
 void InputModule::Init()
 {
@@ -122,12 +121,14 @@ void InputModule::Init()
 	mouseX = 0.0;
 	mouseY = 0.0;
 
-	window = moduleManager->GetModule<WindowModule>()->GetGlfwWindow();
+	windowModule = moduleManager->GetModule<WindowModule>();
 
-	//glfwSetKeyCallback(window, KeyCallback);
+	//glfwSetKeyCallback(windowModule->GetGlfwWindow(), KeyCallback);
 	//glfwSetMouseButtonCallback(window, MouseButtonCallback);
 	//glfwSetCursorPosCallback(window, CursorPositionCallback);
 	//glfwSetJoystickCallback(JoystickCallback);
+
+	ClearAllInputs();
 }
 
 void InputModule::Start()
@@ -144,7 +145,33 @@ void InputModule::Update()
 {
 	Module::Update();
 
-	ProcessInput();
+	//ProcessInput();
+	glfwPollEvents();
+
+	if (!windowModule->HasFocus())
+	{
+		//ClearAllInputs();
+		//return;
+	}
+
+	// Keyboard keys
+	for (auto& keyPair : keys)
+	{
+		if (keyPair.second.down > 0)
+		{
+			++keyPair.second.down;
+		}
+	}
+
+	for (uint32_t i = 0; i < MOUSE_BUTTON_COUNT; ++i)
+	{
+		m_MouseButtonsPressed &= ~(1 << i);
+		m_MouseButtonsReleased &= ~(1 << i);
+		if (m_MouseButtonStates & (1 << i))
+		{
+			m_MouseButtonDrags[i].endLocation = m_MousePosition;
+		}
+	}
 }
 
 void InputModule::PreRender()
@@ -177,40 +204,182 @@ void InputModule::Finalize()
 	Module::Finalize();
 }
 
-i32 InputModule::GetKeyDown(KeyCode keyCode, bool bIgnoreImGui /* = false */) const
+int32_t InputModule::GetKeyDown(Inputs::KeyCode keyCode, bool bIgnoreImGui /* = false */) const
 {
 	if (!bIgnoreImGui && ImGui::GetIO().WantCaptureKeyboard)
 	{
 		return 0;
 	}
 
-	auto iter = m_Keys.find(keyCode);
-	if (iter != m_Keys.end())
+	auto iter = keys.find(keyCode);
+	if (iter != keys.end())
 	{
-		return m_Keys.at(keyCode).down;
+		return keys.at(keyCode).down;
 	}
 
 	return 0;
 }
 
-bool InputModule::GetKeyPressed(KeyCode keyCode, bool bIgnoreImGui /* = false */) const
+bool InputModule::GetKeyPressed(Inputs::KeyCode keyCode, bool bIgnoreImGui /* = false */) const
 {
 	return GetKeyDown(keyCode, bIgnoreImGui) == 1;
 }
 
-bool InputModule::GetKeyReleased(KeyCode keyCode, bool bIgnoreImGui /* = false */) const
+bool InputModule::GetKeyReleased(Inputs::KeyCode keyCode, bool bIgnoreImGui /* = false */) const
 {
 	if (!bIgnoreImGui && ImGui::GetIO().WantCaptureKeyboard)
 	{
 		return false;
 	}
 
-	auto iter = m_Keys.find(keyCode);
-	if (iter != m_Keys.end())
+	auto iter = keys.find(keyCode);
+	if (iter != keys.end())
 	{
-		const Key& key = iter->second;
+		const Inputs::Key& key = iter->second;
 		return key.down == 0 && key.pDown > 0;
 	}
 
 	return false;
+}
+
+void InputModule::ClearAllInputs()
+{
+	ClearMouseInput();
+	ClearKeyboadInput();
+}
+
+void InputModule::ClearMouseInput()
+{
+	m_PrevMousePosition = m_MousePosition = glm::vec2(-1.0f);
+	m_ScrollXOffset = 0.0f;
+	m_ScrollYOffset = 0.0f;
+
+	m_MouseButtonStates = 0;
+	m_MouseButtonsPressed = 0;
+	m_MouseButtonsReleased = 0;
+
+	for (Inputs::MouseDrag& mouseDrag : m_MouseButtonDrags)
+	{
+		mouseDrag.startLocation = glm::vec2{ 0, 0 };
+		mouseDrag.endLocation = glm::vec2{ 0, 0 };
+	}
+
+	windowModule->SetCursorMode(GlfwCursorMode::NORMAL);
+
+	if (ImGui::GetCurrentContext() != nullptr)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		io.MousePos = ImVec2(-FLT_MAX, -FLT_MAX);
+		io.MouseWheel = m_ScrollYOffset;
+	}
+}
+
+void InputModule::ClearKeyboadInput()
+{
+	for (auto& keyPair : keys)
+	{
+		keyPair.second.pDown = 0;
+		keyPair.second.down = 0;
+	}
+}
+
+glm::vec2 InputModule::GetMousePosition() const
+{
+	return m_MousePosition;
+}
+
+void InputModule::KeyCallback(Inputs::KeyCode keyCode, Inputs::KeyAction keyAction, int32_t mods)
+{
+	//FLEX_UNUSED(mods);
+
+	keys[keyCode].pDown = keys[keyCode].down;
+
+	if (keyAction == Inputs::KeyAction::KEY_PRESS)
+	{
+		keys[keyCode].down = 1;
+	}
+	else if (keyAction == Inputs::KeyAction::KEY_REPEAT)
+	{
+		// Ignore repeat events (we're already counting how long the key is down for
+	}
+	else if (keyAction == Inputs::KeyAction::KEY_RELEASE)
+	{
+		keys[keyCode].down = 0;
+	}
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.KeysDown[(int32_t)keyCode] = keys[keyCode].down > 0;
+
+	const bool bCtrlDown = GetKeyDown(Inputs::KeyCode::KEY_LEFT_CONTROL, true) || GetKeyDown(Inputs::KeyCode::KEY_RIGHT_CONTROL, true);
+	const bool bShiftDown = GetKeyDown(Inputs::KeyCode::KEY_LEFT_SHIFT, true) || GetKeyDown(Inputs::KeyCode::KEY_RIGHT_SHIFT, true);
+	const bool bAltDown = GetKeyDown(Inputs::KeyCode::KEY_LEFT_ALT, true) || GetKeyDown(Inputs::KeyCode::KEY_RIGHT_ALT, true);
+	const bool bSuperDown = GetKeyDown(Inputs::KeyCode::KEY_LEFT_SUPER, true) || GetKeyDown(Inputs::KeyCode::KEY_RIGHT_SUPER, true);
+	const bool bModiferDown = (bCtrlDown || bShiftDown || bAltDown || bSuperDown);
+
+	io.KeyCtrl = bCtrlDown;
+	io.KeyShift = bShiftDown;
+	io.KeyAlt = bAltDown;
+	io.KeySuper = bSuperDown;
+
+	//if (!io.WantCaptureKeyboard)
+	//{
+	//	// Call callbacks present in m_ActionCallbacks & m_KeyEventCallbacks according to priorities
+	//	auto actionIter = m_ActionCallbacks.end();
+	//	auto keyEventIter = m_KeyEventCallbacks.begin();
+	//	Action keyPressAction = Action::_NONE;
+	//	ActionEvent actionEvent = keyAction == KeyAction::KEY_PRESS ? ActionEvent::ACTION_TRIGGER : ActionEvent::ACTION_RELEASE;
+
+	//	// TODO: Allow modifiers to be down once supported properly
+	//	if ((keyAction == KeyAction::KEY_PRESS || keyAction == KeyAction::KEY_RELEASE) && !bModiferDown)
+	//	{
+	//		keyPressAction = GetActionFromKeyCode(keyCode);
+	//		if (keyPressAction != Action::_NONE)
+	//		{
+	//			actionIter = m_ActionCallbacks.begin();
+	//		}
+	//	}
+
+	//	bool bEventsInQueue = (actionIter != m_ActionCallbacks.end()) ||
+	//		(keyEventIter != m_KeyEventCallbacks.end());
+	//	while (bEventsInQueue)
+	//	{
+	//		if (actionIter == m_ActionCallbacks.end())
+	//		{
+	//			if (keyEventIter->first->Execute(keyCode, keyAction, mods) == EventReply::CONSUMED)
+	//			{
+	//				break;
+	//			}
+	//			++keyEventIter;
+	//		}
+	//		else if (keyEventIter == m_KeyEventCallbacks.end())
+	//		{
+	//			if (actionIter->first->Execute(keyPressAction, actionEvent) == EventReply::CONSUMED)
+	//			{
+	//				break;
+	//			}
+	//			++actionIter;
+	//		}
+	//		else
+	//		{
+	//			if (actionIter->second >= keyEventIter->second)
+	//			{
+	//				if (actionIter->first->Execute(keyPressAction, actionEvent) == EventReply::CONSUMED)
+	//				{
+	//					break;
+	//				}
+	//				++actionIter;
+	//			}
+	//			else
+	//			{
+	//				if (keyEventIter->first->Execute(keyCode, keyAction, mods) == EventReply::CONSUMED)
+	//				{
+	//					break;
+	//				}
+	//				++keyEventIter;
+	//			}
+	//		}
+
+	//		bEventsInQueue = (actionIter != m_ActionCallbacks.end()) || (keyEventIter != m_KeyEventCallbacks.end());
+	//	}
+	//}
 }
